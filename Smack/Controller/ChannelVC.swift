46456cf6927a5e112fcca83e8e21afc7c6208b45
김영석 <shinerdes@naver.com>
2018-09-06 19:14:54 +0900

    SWReveal

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by 김영석 on 2018. 9. 6..
+//  Copyright © 2018년 김영석. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.width - 60
+        // Do any additional setup after loading the view.
+    }
+
+  
+
+}
